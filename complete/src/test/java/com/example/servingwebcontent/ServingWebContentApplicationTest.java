1fae6d0419a8b86b13b3600514ca78e2a9364b85
antoine-overflow <antoine@DESKTOP-VKFREPH.localdomain>
2022-01-13 17:29:48 +0100

    bug correction

--- complete/src/test/java/com/example/servingwebcontent/ServingWebContentApplicationTest.java
+++ complete/src/test/java/com/example/servingwebcontent/ServingWebContentApplicationTest.java
@@ -20,13 +20,15 @@ import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
+import com.application.AppController;
+
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(controllers = GreetingController.class)
+@WebMvcTest(controllers = AppController.class)
 public class ServingWebContentApplicationTest {
 
 	@Autowired
